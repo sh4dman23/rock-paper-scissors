30eb5be70e9d270e6d61725f4895eb6ed734f103
Shadman <sh4dman0001@gmail.com>
2023-10-19 20:08:22 +0600

    Added an UI to the game

--- script.js
+++ script.js
@@ -1,27 +1,91 @@
-// Maximum number of rounds in a game
-const MAXROUNDS = 5;
+const newGameButton = document.querySelector('#newGame');
+const restartGameButton = document.querySelector('#restartGame');
 
-// Get the player's choice
-function getPlayerChoice(roundNumber) {
-    let playerChoice;
+// Keep buttons disabled until the game starts
+restartGameButton.disabled = true;
+const buttonContainer = document.querySelector('#choiceSelection');
+const buttons = document.querySelectorAll('.choice');
+toggleButtons();
 
-    // Continue looping while user doesnt give a valid answer
-    while (true) {
-        playerChoice = prompt(`Round #${roundNumber}: Choose your weapon. Rock/Paper/Scissors`);
+const statusBox = document.querySelector('#status');
+const resultBox = document.querySelector('#moveResult');
 
-        if (playerChoice == null) {
-            // Game was aborted
-            return 1;
-        }
+const playerScoreElement = document.querySelector('#playerScore');
+const computerScoreElement = document.querySelector('#computerScore');
 
-        playerChoice = playerChoice.trim().toLowerCase();
-        if (['rock', 'paper', 'scissor', 'scissors'].includes(playerChoice)) {
-            break;
-        }
+let playerScore = 0, computerScore = 0, roundsPlayed = 0, gameStarted = false;
+
+newGameButton.addEventListener('click', () => {
+    newGameButton.disabled = true;
+    restartGameButton.disabled = false;
+    startGame();
+    toggleButtons();
+});
+
+restartGameButton.addEventListener('click', () => {
+    startGame();
+});
+
+
+// Play multiple rounds of the game
+buttonContainer.addEventListener('click', (e) => {
+    if (!gameStarted) {
+        return;
     }
 
-    // Also accept scissor as an answer but convert it to scissors
-    return playerChoice !== 'scissor' ? playerChoice : 'scissors';
+    const target = e.target;
+    const playerChoice = target.name;
+    const computerChoice = getComputerChoice();
+    const roundResult = getWinner(playerChoice, computerChoice);
+
+    updateImage('player', playerChoice);
+    updateImage('computer', computerChoice);
+
+    if (roundResult === 'win') {
+        statusBox.textContent = 'You won the round!';
+        resultBox.textContent = `${playerChoice} beats ${computerChoice}!`;
+        playerScoreElement.textContent = ++playerScore;
+    } else if (roundResult === 'loss') {
+        statusBox.textContent = 'You lost the round!';
+        resultBox.textContent = `${playerChoice} was beaten by ${computerChoice}!`;
+        computerScoreElement.textContent = ++computerScore;
+    } else {
+        statusBox.textContent = 'You tied!';
+        resultBox.textContent = `${playerChoice} can\'t beat ${computerChoice}!`;
+    }
+
+    roundsPlayed++;
+
+    // Check for winner
+    if (playerScore === 5 || computerScore === 5) {
+        newGameButton.disabled = false;
+        restartGameButton.disabled = true;
+        toggleButtons();
+
+        let result;
+        if (playerScore === 5) {
+            statusBox.textContent = 'You won the game!';
+            result = 'won';
+        } else {
+            statusBox.textContent = 'You lost the game';
+            result = 'lost';
+        }
+
+        resultBox.textContent = `You ${result} by ${Math.abs(computerScore - playerScore)} points!`;
+    }
+});
+
+function startGame() {
+    playerScore = 0;
+    computerScore = 0;
+    roundsPlayed = 0;
+    gameStarted = true;
+}
+
+function toggleButtons() {
+    buttons.forEach(element => {
+        element.disabled = element.disabled ? false : true;
+    });
 }
 
 // Get the computer's choice
@@ -29,97 +93,45 @@ function getComputerChoice() {
     let choiceNum = Math.floor(Math.random() * 3);
 
     if (choiceNum === 0) {
-        return 'rock';
+        return 'Rock';
     } else if (choiceNum === 1) {
-        return 'paper';
+        return 'Paper';
     } else {
-        return 'scissors';
+        return 'Scissors';
     }
 }
 
 // Get the winner of a single round of rock-paper-scissors
 function getWinner(playerChoice, computerChoice) {
-    if (playerChoice === 'rock') {
-
-        return (computerChoice === 'rock') ? 'tie'
-            : (computerChoice === 'paper') ? 'win'
-            : 'loss';
-    } else if (playerChoice === 'paper') {
-        return (computerChoice === 'rock') ? 'win'
-            : (computerChoice === 'paper') ? 'tie'
+    if (playerChoice === 'Rock') {
+        return (computerChoice === 'Rock') ? 'tie'
+            : (computerChoice === 'Paper') ? 'loss'
+            : 'win';
+    } else if (playerChoice === 'Paper') {
+        return (computerChoice === 'Rock') ? 'win'
+            : (computerChoice === 'Paper') ? 'tie'
             : 'loss';
     } else {
-        return (computerChoice === 'rock') ? 'loss'
-            : (computerChoice === 'paper') ? 'win'
+        return (computerChoice === 'Rock') ? 'loss'
+            : (computerChoice === 'Paper') ? 'win'
             : 'tie';
     }
 }
 
-// Play multiple rounds of the game
-function game() {
-    let winCount = 0, lossCount = 0, tieCount = 0;
+function updateImage(sideToUpdate, choice) {
+    const img = (sideToUpdate === 'player') ? document.querySelector('.last-moves .move-box:nth-child(1)').querySelector('img')
+                : document.querySelector('.last-moves .move-box:nth-child(2)').querySelector('img');
+    img.alt = choice;
 
-    // Initialize logs to an empty list
-    const logs = [];
-
-    console.log('Welcome to Rock, Paper, Shotgun- uh, I meant, Rock, Paper, Scissors!');
-    console.log(`The game has begun. Get the best of ${MAXROUNDS} rounds to win!`);
-
-    // Play Maxround number of rounds
-    for(let roundsPlayed = 0; roundsPlayed < MAXROUNDS; roundsPlayed++) {
-        // Get and store player's choice
-        let playerChoice = getPlayerChoice(roundsPlayed + 1);
-
-        // Abort if playerChoice is empty
-        if (playerChoice === 1) {
-            console.warn('Game was aborted! Type \'game()\' in the console to play again!');
-            return;
-        }
-
-        let computerChoice = getComputerChoice();
-
-        // Get the winner and return result to player
-        const result = getWinner(playerChoice, computerChoice);
-
-        // Title-case choices
-        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-
-        logs[roundsPlayed] = {
-            'player': playerChoice,
-            'computer': computerChoice,
-            'result': result.charAt(0).toUpperCase() + result.slice(1)
-        };
-
-        if (result === 'win') {
-            winCount++;
-            console.log(`You win! ${playerChoice} beats ${computerChoice}!`);
-        } else if (result === 'loss') {
-            lossCount++;
-            console.log(`You lose! ${computerChoice} beats ${playerChoice}!`);
-        } else {
-            tieCount++;
-            console.log(`Tie! ${playerChoice} can\'t beat ${computerChoice}`);
-        }
+    switch(choice) {
+        case 'Rock':
+            img.src = './images/rock.png';
+            break;
+        case 'Paper':
+            img.src = './images/paper.png';
+            break;
+        case 'Scissors':
+            img.src = './images/scissors.png';
+            break;
     }
-
-    // Provide overall result
-    if (winCount > lossCount) {
-        console.log('You won the game!');
-    } else {
-        console.log('You lost the game! Play again by typing \'game()\' in the console.');
-    }
-
-    // Provide details
-    console.log('Details:\n');
-    console.log(`Wins: ${winCount} \nLosses: ${lossCount} \nTies: ${tieCount}`);
-    for (let i = 0; i < MAXROUNDS; i++) {
-        console.log(`    Round #${i + 1}:`);
-        console.log(`        Your Choice: ${logs[i]['player']}`);
-        console.log(`        Computer's Choice: ${logs[i]['computer']}`);
-        console.log(`        Result: ${logs[i]['result']}`);
-    }
-
-    return 'Thanks for playing!';
 }
-
